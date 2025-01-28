c54222b1de6945cd138c93b4f59363bb26ee243e
Tajeshwar Singh <147947134+tajeshwar1@users.noreply.github.com>
2025-01-28 18:07:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {useState ,useEffect} from "react";
 import MovieCard from './movieCard';
 import './App.css';
 import SearchIcon from './search.svg';
-const API_URL = 'http://www.omdbapi.com?apikey=afeac97c';
+const API_URL = 'https://www.omdbapi.com?apikey=afeac97c';
 
 
 const App = ()=>{
